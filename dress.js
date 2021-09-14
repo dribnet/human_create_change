a3d98594f8d908f28e693be3ae2bfebcb785655d
Tom White <tom@sixdozen.com>
2021-09-14 20:15:13 +1200

    cleanup

--- dress.js
+++ dress.js
@@ -1,11 +1,45 @@
 const bgcolor = "#e0d1bc";
+let bgImage = null;
+let lastX = null, lastY = null;
+
+function preload() {
+  bgImage = loadImage('hips.png');
+}
 
 function setup() {
   let canvas = createCanvas(960, 960);
   canvas.parent('canvasContainer');
+  do_clear();
+  frameRate(60);
+}
+
+function do_clear() {
+  background(bgcolor);
+  image(bgImage, 0, 0);
+}
+
+function mousePressed() {
+  lastX = mouseX;
+  lastY = mouseY;
 }
 
 function draw() {
-  background(bgcolor);
-  ellipse(mouseX,mouseY,80,80);
+  if (mouseIsPressed) {
+    let diff = 4 * Math.abs(lastX - mouseX) + Math.abs(lastY - mouseY);
+    ellipse(mouseX,mouseY,diff,diff);
+    ellipse((width-mouseX),mouseY,diff,diff);
+    lastX = mouseX;
+    lastY = mouseY;
+  }
+}
+
+function keyTyped() {
+  if (key == ' ') {
+    do_clear();
+    return false;
+  }
+  if (key == 's') {
+    saveImage();
+    return false;
+  }  
 }
