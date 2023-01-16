5abb1c1af65cfa27966b19fa4eeb22cfb8611496
Tom White <tom@sixdozen.com>
2023-01-16 05:53:06 -0500

    first version with three words

--- human.js
+++ human.js
@@ -2,11 +2,19 @@ let bgImage = null;
 const bgColor = "#33362f"
 
 function preload() {
-  bgImage = loadImage('human3m_example1.jpg');
+  bgImage = loadImage('human3m_full_8m.jpg');
+  font = loadFont('Avenir_Book.ttf');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+}
+
+
+let main_words = {
+  "HUMAN": [187, 591],
+  "CREATE": [414, 218],
+  "CHANGE": [737, 520]
 }
 
 function draw() {
@@ -17,6 +25,19 @@ function draw() {
   let pixel_width = int((bgImage.width/bgImage.height) * height);
   let bg_start_x = int((width-pixel_width)/2)
   image(bgImage, bg_start_x, 0, pixel_width, pixel_height);
+
+  fill('#ffffff');
+  textFont(font);
+  textAlign(CENTER);
+  textStyle(BOLD);
+  textSize(26);
+
+  for (const key in main_words) {
+    let coords = main_words[key];
+    let hx = bg_start_x + int(coords[0]/1000 * pixel_width);
+    let hy = int(coords[1]/1000 * pixel_height);
+    text(key, hx, hy);
+  }
 }
 
 function windowResized() {
