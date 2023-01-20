de84426d7ac2aa24f42108c6b479743751bde026
Tom White <tom@sixdozen.com>
2023-01-20 10:36:55 -0500

    added test word cities

--- human.js
+++ human.js
@@ -2,16 +2,60 @@ let bgImage = null;
 const bgColor = "#33362f";
 let cur_zoom = 0;
 let birthday = 0;
-let millis_per_step = 2000;
+let millis_per_step = 15000;
+let test_labels = null;
+let test_table = null;
 
 function preload() {
   bgImage = loadImage('human3m_full_8m.jpg');
   font = loadFont('Avenir_Book.ttf');
+  train_labels = loadStrings('all_change_create_human_train_filtered.tsv');
+  train_table = loadTable('create_change_human_train2_1_train2_patch2_points_train.csv', 'csv');
+  test_labels = loadStrings('all_change_create_human_artai_filtered.tsv');
+  test_table = loadTable('create_change_human_train2_1_train3_patch1_points_test.csv', 'csv');
 }
+
+let tour = [
+  [500, 500, 0],
+  [500, 500, 1],
+  [187, 591, 1.5],
+  [414, 218, 1.5],
+  [737, 620, 1.5]
+];
+
+let main_words = {
+  "HUMAN": [187, 591],
+  "CREATE": [414, 218],
+  "CHANGE": [737, 520]
+};
+
+let test_words = {};
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   birthday = millis();
+
+  let min_x = -5.8;
+  let max_x = 15.67;
+  let min_y = -9.7;
+  let max_y = 16.7;
+
+  let num_rows = test_table.getRowCount();
+  for(let i=0; i<num_rows; i++) {
+    let x = float(test_table.getString(i, 0));
+    let y = float(test_table.getString(i, 1));
+    let city_string = test_labels[i];
+    // map to screen space
+    let scaled_x = map(x, min_x, max_x, 0, 1000);
+    let scaled_y = map(y, min_y, max_y, 1000, 0);
+    test_words[city_string] = [scaled_x, scaled_y];
+
+    if(i==0 || i==20) {
+      tour.push([scaled_x, scaled_y, 2.0]);
+      tour.push([scaled_x, scaled_y, 3.0]);
+      tour.push([scaled_x, scaled_y, 2.0]);
+    }
+  }
 }
 
 // acuSerpf copied almost verbatium from acu :-)
@@ -21,12 +65,6 @@ function acuSerpf(t, a, b) {
   else factor = 1 - 2.0 * (1.0 - t) * (1.0 - t);
   return (a + factor * (b - a));
 }
-
-let main_words = {
-  "HUMAN": [187, 591],
-  "CREATE": [414, 218],
-  "CHANGE": [737, 520]
-};
 
 let zoom_1_words = {
   "human rights": [330, 560],
@@ -57,14 +95,6 @@ let zoom_2_words = {
   "change a name": [786, 545],
 };
 
-let tour = [
-  [500, 500, 0],
-  [500, 500, 1],
-  [187, 591, 1.5],
-  [414, 218, 1.5],
-  [737, 620, 1.5]
-];
-
 function get_tour_location() {
   let age = millis() - birthday;
   let steps = age / millis_per_step;
@@ -85,6 +115,7 @@ function get_tour_location() {
 }
 
 function render_words(words, dx, dy, dWidth, dHeight, source_startx, source_starty, source_width, source_height) {
+  textAlign(CENTER);
   for (const key in words) {
     let coords = words[key];
     let im_x = map(coords[0], 0, 1000, 0, bgImage.width);
@@ -94,6 +125,40 @@ function render_words(words, dx, dy, dWidth, dHeight, source_startx, source_star
       let hx = map(im_x, source_startx, source_startx + source_width, dx, dx+dWidth);
       let hy = map(im_y, source_starty, source_starty + source_height, dy, dy+dHeight);
       text(key, hx, hy);
+    }
+  }  
+}
+
+function render_words_paragraph(words, word_size, dx, dy, dWidth, dHeight, source_startx, source_starty, source_width, source_height) {
+  textWrap(WORD);
+  textAlign(LEFT);
+  for (const key in words) {
+    let coords = words[key];
+    let im_x = map(coords[0], 0, 1000, 0, bgImage.width);
+    let im_y = map(coords[1], 0, 1000, 0, bgImage.height);
+    if (im_x+100 > source_startx && im_x-100 < source_startx + source_width &&
+        im_y+100 > source_starty && im_y-100 < source_starty + source_height) {
+      let hx = map(im_x, source_startx, source_startx + source_width, dx, dx+dWidth);
+      let hy = map(im_y, source_starty, source_starty + source_height, dy, dy+dHeight);
+      fill(0,0,0,100);
+      rect(hx-100, hy-100, 1000, word_size*13);
+      fill(255);
+      text(key, hx, hy, 800);
+    }
+  }  
+}
+
+function render_ellipses(words, dot_size, dx, dy, dWidth, dHeight, source_startx, source_starty, source_width, source_height) {
+  for (const key in words) {
+    let coords = words[key];
+    let im_x = map(coords[0], 0, 1000, 0, bgImage.width);
+    let im_y = map(coords[1], 0, 1000, 0, bgImage.height);
+    if (im_x+100 > source_startx && im_x-100 < source_startx + source_width &&
+        im_y+100 > source_starty && im_y-100 < source_starty + source_height) {
+      let hx = map(im_x, source_startx, source_startx + source_width, dx, dx+dWidth);
+      let hy = map(im_y, source_starty, source_starty + source_height, dy, dy+dHeight);
+      ellipse(hx, hy, dot_size);
+      // text(key, hx, hy);
     }
   }  
 }
@@ -190,13 +255,12 @@ function draw() {
   let full_text_size = int(0.04 * dHeight);
   let smaller_text_size = int(0.8 * full_text_size);
   let smallest_text_size = int(0.7 * full_text_size);
+  let city_text_size = int(0.5 * full_text_size);
 
-  textSize(full_text_size);
-  render_words(main_words, dx, dy, dWidth, dHeight, source_startx, source_starty, source_width, source_height);
   textSize(smaller_text_size);
   if (cur_zoom >= 0.7) {
     if (cur_zoom < 0.8) {
-      let word_size = map(cur_zoom, 0.7, 0.8, 1, 26);
+      let word_size = map(cur_zoom, 0.7, 0.8, 1, smaller_text_size);
       textSize(word_size);
     }
     render_words(zoom_1_words, dx, dy, dWidth, dHeight, source_startx, source_starty, source_width, source_height);
@@ -204,10 +268,33 @@ function draw() {
   textSize(smallest_text_size);
   if (cur_zoom >= 1.4) {
     if (cur_zoom < 1.5) {
-      let word_size = map(cur_zoom, 1.4, 1.5, 1, 22);
+      let word_size = map(cur_zoom, 1.4, 1.5, 1, smallest_text_size);
       textSize(word_size);
     }
     render_words(zoom_2_words, dx, dy, dWidth, dHeight, source_startx, source_starty, source_width, source_height);
+  }
+
+  if (cur_zoom >= 1.0) {
+    fill(232, 0, 50);
+    noStroke();
+    let city_dot_size = int(0.01 * dHeight);
+    if (cur_zoom < 1.1) {
+      city_dot_size = map(cur_zoom, 1.0, 1.1, 1, city_dot_size);
+    }
+    render_ellipses(test_words, city_dot_size, dx, dy, dWidth, dHeight, source_startx, source_starty, source_width, source_height);
+  }
+
+  fill('#ffffff');
+  textSize(full_text_size);
+  render_words(main_words, dx, dy, dWidth, dHeight, source_startx, source_starty, source_width, source_height);
+
+  if (cur_zoom >= 2.2) {
+    let word_size = city_text_size;
+    if (cur_zoom < 2.3) {
+      word_size = map(cur_zoom, 2.2, 2.3, 1, city_text_size);
+    }
+    textSize(word_size);
+    render_words_paragraph(test_words, word_size, dx, dy, dWidth, dHeight, source_startx, source_starty, source_width, source_height);
   }
 }
 
